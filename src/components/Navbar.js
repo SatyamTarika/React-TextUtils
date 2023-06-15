a35875e8e78bef909f6c78e534ac4769c29b6b4f
SatyamTarika10 <tarikasatyam1999arora@gmail.com>
2023-06-15 22:40:23 +0530

    First Changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid justify-content-center">
+                    <a className="navbar-brand" href="/">
+                        React App TextUtils
+                    </a>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
